fcbe3896b236bbeb0f3a4f32327ba042482f2562
ola <olaidealaka12@gmail.com>
2023-06-02 22:22:09 +0100

    display other models in the admin

--- resume/admin.py
+++ resume/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from resume.models import Bio, Experience, Project
+from resume.models import Bio, Education, Experience, Language, Project, Skill
 
 # Register your models here.
 
 admin.site.register(Bio)
+admin.site.register(Education)
 admin.site.register(Experience)
+admin.site.register(Language)
 admin.site.register(Project)
+admin.site.register(Skill)
